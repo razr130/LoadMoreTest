fd478d1944804b948edb3552e20931e3b89694ba
SpaceColony <jovfernando@yahoo.com>
2023-02-01 12:19:40 +0700

    add public class

---
+++ app/src/main/java/com/example/loadmoretest/PublicClass.kt
@@ -0,0 +1,11 @@
+package com.example.loadmoretest
+
+import android.content.Context
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class PublicClass(context: Context?) {
+    fun printSomething(activity: AppCompatActivity) {
+        Toast.makeText(activity.applicationContext, "Hello from example", Toast.LENGTH_SHORT).show()
+    }
+}
